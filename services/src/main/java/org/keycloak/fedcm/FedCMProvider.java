88b3ed0b6733989630bd19c10c29d1ea010c5250
Simon Vacek <simonvacky@email.cz>
2024-03-27 10:34:14 +0100

    FedCM header condition

--- services/src/main/java/org/keycloak/fedcm/FedCMProvider.java
+++ services/src/main/java/org/keycloak/fedcm/FedCMProvider.java
@@ -1,6 +1,12 @@
 package org.keycloak.fedcm;
 
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.keycloak.models.KeycloakSession;
@@ -54,27 +60,37 @@ public class FedCMProvider implements RealmResourceProvider {
     @GET
     @Path("accounts")
     @Produces(MediaType.APPLICATION_JSON)
-    public Map<String, List<Map<String, Object>>> fetchAccountsList(@HeaderParam("Sec-Fetch-Dest") String secFetchDest) {
+    public Map<String, Object> fetchAccountsList(@HeaderParam("Sec-Fetch-Dest") String secFetchDest) {
         // todo get picture URL
         // todo store somewhere approved_clients
         // todo what to put in domain_hints
-        Map<String, List<Map<String, Object>>> accList = new HashMap<>();
-        List<Map<String, Object>> accounts = new ArrayList<>();
-        Map<String, Object> account = new HashMap<>();
+
+        Map<String, Object> accList = new HashMap<>();
+        if (secFetchDest.equals("webidentity")) {
+            List<Map<String, Object>> accounts = new ArrayList<>();
+            Map<String, Object> account = new HashMap<>();
 
 
-        AuthResult authResult = (new AuthenticationManager()).authenticateIdentityCookie(session, session.getContext().getRealm());
-        UserModel userModel = authResult.getUser();
-        account.put("id", userModel.getId());
-        account.put("given_name", userModel.getFirstName());
-        account.put("name", userModel.getFirstName() + ' ' + userModel.getLastName());
-        account.put("email", userModel.getEmail());
-        account.put("approved_clients", new ArrayList<String>() {{ add("123"); add("456"); }});
-        account.put("login_hints", new ArrayList<String>() {{ add(userModel.getEmail()); }});
+            AuthResult authResult = (new AuthenticationManager()).authenticateIdentityCookie(session, session.getContext().getRealm());
+            UserModel userModel = authResult.getUser();
+            account.put("id", userModel.getId());
+            account.put("given_name", userModel.getFirstName());
+            account.put("name", userModel.getFirstName() + ' ' + userModel.getLastName());
+            account.put("email", userModel.getEmail());
+            account.put("approved_clients", new ArrayList<String>() {{
+                add("123");
+                add("456");
+            }});
+            account.put("login_hints", new ArrayList<String>() {{
+                add(userModel.getEmail());
+            }});
 
-        accounts.add(account);
-        accList.put("accounts", accounts);
-
+            accounts.add(account);
+            accList.put("accounts", accounts);
+        }
+        else {
+            accList.put("error", "Sec-Fetch-Dest header is not set to webidentity");
+        }
         return accList;
     }
 
@@ -83,13 +99,17 @@ public class FedCMProvider implements RealmResourceProvider {
     @Produces(MediaType.APPLICATION_JSON)
     public Map<String, Object> fetchClientMetadata(@HeaderParam("Sec-Fetch-Dest") String secFetchDest, @QueryParam("client_id") int client_id) {
         Map<String, Object> metadata = new HashMap<>();
-        if(client_id == 123) {
-            metadata.put("privacy_policy_url", "https://www.seznam.cz/");
-            metadata.put("terms_of_service_url", "https://www.seznam.cz/");
+        if(secFetchDest.equals("webidentity")) {
+            if (client_id == 123) {
+                metadata.put("privacy_policy_url", "https://www.seznam.cz/");
+                metadata.put("terms_of_service_url", "https://www.seznam.cz/");
+            } else {
+                metadata.put("privacy_policy_url", "https://www.google.com/");
+                metadata.put("terms_of_service_url", "https://www.google.com/");
+            }
         }
         else {
-            metadata.put("privacy_policy_url", "https://www.google.com/");
-            metadata.put("terms_of_service_url", "https://www.google.com/");
+            metadata.put("error", "Sec-Fetch-Dest header is not set to webidentity");
         }
         return metadata;
     }
@@ -98,9 +118,13 @@ public class FedCMProvider implements RealmResourceProvider {
     @Path("id_assert")
     @Produces(MediaType.APPLICATION_JSON)
     public Map<String, String> fetchIdentityAssertion(@HeaderParam("Sec-Fetch-Dest") String secFetchDest, @FormParam("account_id") String account_id, @FormParam("client_id") String client_id, @FormParam("nonce") String nonce, @QueryParam("disclosure_text_shown") boolean disclosure_text_shown) {
-
         Map<String, String> token = new HashMap<>();
-        token.put("token", "not-a-real-token-just-yet-123");
+        if (secFetchDest.equals("webidentity")) {
+            token.put("token", "not-a-real-token-just-yet-123");
+        }
+        else {
+            token.put("error", "Sec-Fetch-Dest header is not set to webidentity");
+        }
         return token;
     }
 
@@ -108,18 +132,22 @@ public class FedCMProvider implements RealmResourceProvider {
     @Path("disconnect")
     @Produces(MediaType.APPLICATION_JSON)
     public Response disconnect(@HeaderParam("Sec-Fetch-Dest") String secFetchDest, @HeaderParam("Origin") String client_origin, @FormParam("client_id") String client_id, @FormParam("account_hint") String account_hint) {
-        AuthResult authResult = (new AuthenticationManager()).authenticateIdentityCookie(session, session.getContext().getRealm());
-        UserModel userModel = authResult.getUser();
-        Map<String, String> id = new HashMap<>();
-        id.put("account_id", userModel.getId());
+        if (secFetchDest.equals("webidentity")) {
+            Map<String, String> id = new HashMap<>();
+            AuthResult authResult = (new AuthenticationManager()).authenticateIdentityCookie(session, session.getContext().getRealm());
+            UserModel userModel = authResult.getUser();
+            id.put("account_id", userModel.getId());
 
-        Response.ResponseBuilder rb = Response.ok(id);
-        Response rtn_id = rb.header("Access-Control-Allow-Origin", client_origin)
-                .type(MediaType.APPLICATION_JSON)
-                .header("Access-Control-Allow-Credentials", true)
-                .build();
+            Response.ResponseBuilder rb = Response.ok(id);
+            return rb.header("Access-Control-Allow-Origin", client_origin)
+                    .type(MediaType.APPLICATION_JSON)
+                    .header("Access-Control-Allow-Credentials", true)
+                    .build();
 
-        return rtn_id;
+        }
+        else {
+            return Response.serverError().build();
+        }
     }
 
     @Override
