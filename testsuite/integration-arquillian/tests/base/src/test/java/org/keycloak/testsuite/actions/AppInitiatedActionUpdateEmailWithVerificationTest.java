3a91907bb29c12c45112f794dd8c72414c46ef20
Simon Vacek <simonvacky@email.cz>
2024-04-17 15:58:45 +0200

    Fix getting attribute link

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/actions/AppInitiatedActionUpdateEmailWithVerificationTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/actions/AppInitiatedActionUpdateEmailWithVerificationTest.java
@@ -173,7 +173,7 @@ public class AppInitiatedActionUpdateEmailWithVerificationTest extends AbstractA
 		assertEquals(String.format("The account email has been successfully updated to %s.", "new@localhost"), infoPage.getInfo());
 		//Issue #15136
 		final WebElement backToApplicationLink = driver.findElement(By.linkText("« Back to Application"));
-		assertThat(backToApplicationLink.toString(), Matchers.containsString("/auth/realms/master/app/auth"));
+		assertThat(backToApplicationLink.getAttribute("href"), Matchers.containsString("/auth/realms/master/app/auth"));
 
 		events.expect(EventType.UPDATE_EMAIL)
 				.detail(Details.PREVIOUS_EMAIL, "test-user@localhost")
